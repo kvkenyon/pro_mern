c4fb5af893921cb64ef6dd78daf864d633cd0c33
Kevin Kenyon <kevin@Kevins-MacBook-Pro.local>
2017-07-11 23:17:10 -0500

    Complete Chapter 3

--- pro_mern/src/App.jsx
+++ pro_mern/src/App.jsx
@@ -8,10 +8,50 @@ class IssueFilter extends React.Component {
   };
 }
 
+class IssueRow extends React.Component {
+  render() {
+    const issue = this.props.issue;
+    return (
+      <tr>
+        <td>{issue.id}</td>
+        <td>{issue.status}</td>
+        <td>{issue.owner}</td>
+        <td>{issue.created.toDateString()}</td>
+        <td>{issue.effort}</td>
+        <td>{issue.completionDate ? issue.completionDate.toDateString() : ''}</td>
+        <td>{issue.title}</td>
+      </tr>
+    );
+  };
+}
+
+IssueRow.propTypes = {
+  issue: React.PropTypes.object
+};
+
+IssueRow.defaultProps = {
+  issue_title: '$ no title $',
+}
+
 class IssueTable extends React.Component {
   render() {
+    const borderedStyle = {border: "1px solid silver", padding: 6};
+    const issueRows = this.props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />);
     return (
-      <div>This is a placeholder for the issue table.</div>
+      <table className="bordered-table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Status</th>
+            <th>Owner</th>
+            <th>Created</th>
+            <th>Effort</th>
+            <th>Completion Date</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>{issueRows}</tbody>
+      </table>
     );
   };
 }
@@ -24,6 +64,19 @@ class IssueAdd extends React.Component {
   };
 }
 
+const issues = [
+  {
+    id: 1, status: 'Open', owner: 'Ravan',
+    created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
+    title: 'Error in console when clicking Add',
+  },
+  {
+    id: 2, status: 'Assigned', owner: 'Eddie',
+    created: new Date('2016-08-16'), effort: 14, completionDate: new Date('2016-08-30'),
+    title: 'Missing bottom border on panel',
+  },
+];
+
 class IssueList extends React.Component {
   render() {
     return(
@@ -31,7 +84,7 @@ class IssueList extends React.Component {
         <h1>Issue Tracker</h1>
         <IssueFilter />
         <hr />
-        <IssueTable />
+        <IssueTable issues={issues} />
         <hr />
         <IssueAdd />
       </div>
