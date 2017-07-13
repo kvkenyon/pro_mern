7b4d5ae0d8c6d98e80c456460b71b8fefd29ae9f
Kevin W. Kenyon <kwkenyon@us.ibm.com>
2017-07-13 16:49:09 -0500

    Add apis and api calls from components

--- pro_mern/src/App.jsx
+++ pro_mern/src/App.jsx
@@ -67,16 +67,37 @@ class IssueList extends React.Component {
 
   loadData() {
     console.log("Loading data..");
-    setTimeout(() => {
-        this.setState({issues: issues});
-    }, 500);
+    fetch('/api/issues').then(response =>
+        response.json()
+    ).then(data => {
+        console.log("Total count of records: ", data._metadata.total_count);
+        data.records.forEach(issue => {
+            issue.created = new Date(issue.created);
+            if (issue.completionDate) {
+                issue.completionDate = new Date(issue.completionDate);
+            }
+        });
+        this.setState({issues: data.records});
+    }).catch( err => {
+        console.log(err);
+    });
   }
 
   createIssue(newIssue) {
-    const newIssues = this.state.issues.slice();
-    newIssue.id = this.state.issues.length + 1;
-    newIssues.push(newIssue);
-    this.setState({issues: newIssues});
+      fetch('/api/issues', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(newIssue)
+      }).then(response => response.json()
+      ).then(updatedIssue => {
+        updatedIssue.created = new Date(updatedIssue.created);
+        if(updatedIssue.completionDate)
+            updatedIssue.completionDate = new Date(updatedIssue.completionDate);
+      const newIssues = this.state.issues.concat(updatedIssue);
+      this.setState({issues: newIssues});
+      }).catch( err => {
+        alert("Error in sending data to server:" + err.message);
+      });
   }
 
   render() {
